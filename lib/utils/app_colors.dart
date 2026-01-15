c5ea64c7d45a47ef23320c49486db40748805209
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-01-15 15:54:46 +0200

    added splash screen added home screen

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainColor = Color(0xFF0E3A99);
+  static const Color disableColor = Color(0xFFB9B9B9);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color backgroundColor = Color(0xFFF4F7FF);
+  static const Color mainTextColor = Color(0xFF1C1C1C);
+  static const Color strokeColor = Color(0xFFF0F0F0);
+  static const Color secTextColor = Color(0xFF686868);
+  static const Color successColor = Color(0xFF2ECC71);
+  static const Color warningColor = Color(0xFFF2994A);
+  static const Color redColor = Color(0xFFFF3232);
+  static const Color bgDarkModeColor = Color(0xFF000F30);
+  static const Color secTextDarkModeColor = Color(0xFFD6D6D6);
+  static const Color strokeDarkColor = Color(0xFF002D8F);
+  static const Color mainDarkModeColor = Color(0xFF457AED);
+  static const Color inputsColor = Color(0xFF001440);
+}
---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:evently/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    textTheme: TextTheme(),
+    scaffoldBackgroundColor: AppColors.backgroundColor,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+
+      backgroundColor: AppColors.whiteColor,
+      selectedIconTheme: IconThemeData(color: AppColors.mainColor),
+      unselectedIconTheme: IconThemeData(color: AppColors.disableColor),
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: AppColors.mainColor,
+      shape: StadiumBorder()
+
+    )
+  );
+  static final ThemeData darkTheme = ThemeData(
+    textTheme: TextTheme(),
+    scaffoldBackgroundColor: AppColors.bgDarkModeColor,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+
+      backgroundColor: AppColors.bgDarkModeColor,
+      selectedIconTheme: IconThemeData(color: AppColors.mainDarkModeColor),
+      unselectedIconTheme: IconThemeData(color: AppColors.disableColor),
+    ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: AppColors.mainDarkModeColor,
+          shape: StadiumBorder()
+      ),
+  );
+}
