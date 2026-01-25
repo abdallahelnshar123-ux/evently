89cc593c856c3ae8e9feae2abfefe406fe0021bd
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-01-25 13:59:52 +0200

    ..

--- lib/home_screen/tabs/profile_tab/profile_tab.dart
+++ lib/home_screen/tabs/profile_tab/profile_tab.dart
@@ -2,11 +2,14 @@ import 'package:evently/home_screen/tabs/profile_tab/widget/language_widget.dart
 import 'package:evently/home_screen/tabs/profile_tab/widget/profile_item_widget.dart';
 import 'package:evently/home_screen/tabs/profile_tab/widget/switch_widget.dart';
 import 'package:evently/utils/app_assets.dart';
+import 'package:evently/utils/app_routes.dart';
 import 'package:evently/utils/screen_size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileTab extends StatelessWidget {
+  const ProfileTab({super.key});
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -26,29 +29,32 @@ class ProfileTab extends StatelessWidget {
               ),
               Text(
                 'Abdallah Elnshar',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .titleMedium,
+                style: Theme.of(context).textTheme.titleMedium,
                 textAlign: TextAlign.center,
               ),
               Text(
                 'abdallahelnshar@gmail.com',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .labelSmall,
+                style: Theme.of(context).textTheme.labelSmall,
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: context.height * 0.030),
               ProfileItemWidget(text: 'dark_mode', itemWidget: SwitchWidget()),
-              ProfileItemWidget(
-                text: 'language',
-                itemWidget: LanguageWidget(),
-              ),
+              ProfileItemWidget(text: 'language', itemWidget: LanguageWidget()),
               ProfileItemWidget(
                 text: 'logout',
-                itemWidget: SvgPicture.asset(AppAssets.logoutIcon, width: 30,),
+                itemWidget: IconButton(
+                  style: IconButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(
+                      context,
+                      AppRoutes.loginRouteName,
+                    );
+                  },
+                  icon: SvgPicture.asset(AppAssets.logoutIcon, width: 30),
+                ),
               ),
             ],
           ),
