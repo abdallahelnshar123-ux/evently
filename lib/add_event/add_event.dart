235b057f685c9cbc6e3c526f6a4c087cbf90f8c0
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-01-29 15:35:00 +0200

    ..

--- lib/add_event/add_event.dart
+++ lib/add_event/add_event.dart
@@ -49,182 +49,184 @@ class _AddEventScreenState extends State<AddEventScreen> {
     selectedImage = context.isLight
         ? data.eventImagesLight[selectedIndex]
         : data.eventImagesDark[selectedIndex];
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          context.tr('add_event'),
-          style: Theme.of(context).textTheme.titleSmall!.copyWith(
-            color: context.isLight
-                ? AppColors.mainTextColor
-                : AppColors.whiteColor,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            context.tr('add_event'),
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(
+              color: context.isLight
+                  ? AppColors.mainTextColor
+                  : AppColors.whiteColor,
+            ),
+          ),
+          leading: BackButtonWidget(
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
-        leading: BackButtonWidget(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(context.width * 0.04),
-        child: Form(
-          key: formKey,
-          child: Column(
-            spacing: context.height * 0.005,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                height: context.height * 0.24,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(selectedImage),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(context.width * 0.04),
+          child: Form(
+            key: formKey,
+            child: Column(
+              spacing: context.height * 0.005,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: context.height * 0.24,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(selectedImage),
+                    ),
+                    border: Border.all(
+                      color: context.isLight
+                          ? AppColors.strokeColor
+                          : AppColors.strokeDarkColor,
+                    ),
+                    borderRadius: BorderRadius.circular(24),
                   ),
-                  border: Border.all(
-                    color: context.isLight
-                        ? AppColors.strokeColor
-                        : AppColors.strokeDarkColor,
-                  ),
-                  borderRadius: BorderRadius.circular(24),
                 ),
-              ),
-              DefaultTabController(
-                length: data.eventsNameList.length,
-                child: TabBar(
-                  onTap: (index) {
-                    selectedIndex = index;
-                    selectedEventName = data.eventsNameList[selectedIndex];
-                    setState(() {});
+                DefaultTabController(
+                  length: data.eventsNameList.length,
+                  child: TabBar(
+                    onTap: (index) {
+                      selectedIndex = index;
+                      selectedEventName = data.eventsNameList[selectedIndex];
+                      setState(() {});
+                    },
+                    tabAlignment: TabAlignment.start,
+                    padding: EdgeInsets.symmetric(
+                      vertical: context.height * 0.029,
+                    ),
+                    isScrollable: true,
+                    dividerColor: AppColors.transparentColor,
+                    indicatorColor: AppColors.transparentColor,
+                    labelPadding: EdgeInsets.symmetric(
+                      horizontal: context.width * 0.01,
+                    ),
+                    tabs: data.eventsNameList
+                        .map(
+                          (eventName) => TabBarWidget(
+                            icon:
+                                selectedIndex ==
+                                    data.eventsNameList.indexOf(eventName)
+                                ? data.selectedIcons[data.eventsNameList
+                                      .indexOf(eventName)]
+                                : data.unselectedIcons[data.eventsNameList
+                                      .indexOf(eventName)],
+                            isSelected:
+                                selectedIndex ==
+                                data.eventsNameList.indexOf(eventName),
+                            eventName: eventName,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                Text(
+                  context.tr('title'),
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+                CustomTextField(
+                  onChanged: (text) {
+                    eventTitle = text;
                   },
-                  tabAlignment: TabAlignment.start,
-                  padding: EdgeInsets.symmetric(
-                    vertical: context.height * 0.029,
+                  validator: (text) {
+                    if (text?.trim().isEmpty ?? true) {
+                      return context.tr('please_enter_event_title');
+                    }
+                    return null;
+                  },
+
+                  dataStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    decoration: TextDecoration.none,
                   ),
-                  isScrollable: true,
-                  dividerColor: AppColors.transparentColor,
-                  indicatorColor: AppColors.transparentColor,
-                  labelPadding: EdgeInsets.symmetric(
-                    horizontal: context.width * 0.01,
+                  filled: true,
+                  fillColor: context.isLight
+                      ? AppColors.whiteColor
+                      : AppColors.inputsColor,
+
+                  hintText: context.tr('search_for_event'),
+                  hintStyle: Theme.of(context).textTheme.labelSmall,
+
+                  errorBorderColor: AppColors.redColor,
+                  generalBorderColor: context.isLight
+                      ? AppColors.strokeColor
+                      : AppColors.strokeDarkColor,
+                ),
+                SizedBox(height: context.height * 0.004),
+                Text(
+                  context.tr('event_title'),
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+                CustomTextField(
+                  validator: (text) {
+                    if (text?.trim().isEmpty ?? true) {
+                      return context.tr('please_enter_event_description');
+                    }
+                    return null;
+                  },
+                  onChanged: (text) {
+                    eventDescription = text;
+                  },
+                  maxLines: 6,
+                  dataStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    decoration: TextDecoration.none,
                   ),
-                  tabs: data.eventsNameList
-                      .map(
-                        (eventName) => TabBarWidget(
-                          icon:
-                              selectedIndex ==
-                                  data.eventsNameList.indexOf(eventName)
-                              ? data.selectedIcons[data.eventsNameList.indexOf(
-                                  eventName,
-                                )]
-                              : data.unselectedIcons[data.eventsNameList
-                                    .indexOf(eventName)],
-                          isSelected:
-                              selectedIndex ==
-                              data.eventsNameList.indexOf(eventName),
-                          eventName: eventName,
-                        ),
-                      )
-                      .toList(),
+                  filled: true,
+                  fillColor: context.isLight
+                      ? AppColors.whiteColor
+                      : AppColors.inputsColor,
+
+                  hintText: context.tr('event_description'),
+                  hintStyle: Theme.of(context).textTheme.labelSmall,
+
+                  errorBorderColor: AppColors.redColor,
+                  generalBorderColor: context.isLight
+                      ? AppColors.strokeColor
+                      : AppColors.strokeDarkColor,
                 ),
-              ),
-              Text(
-                context.tr('title'),
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              CustomTextField(
-                onChanged: (text) {
-                  eventTitle = text;
-                },
-                validator: (text) {
-                  if (text?.trim().isEmpty ?? true) {
-                    return context.tr('please_enter_event_title');
-                  }
-                  return null;
-                },
+                SizedBox(height: context.height * 0.004),
 
-                dataStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  decoration: TextDecoration.none,
+                DateTimeWidget.date(
+                  onPressed: pickDate,
+                  hyperText: selectedDate == null
+                      ? context.tr('choose_date')
+                      : DateFormat(
+                          'MMM d, yyyy',
+                          context.locale.toString(),
+                        ).format(selectedDate!),
                 ),
-                filled: true,
-                fillColor: context.isLight
-                    ? AppColors.whiteColor
-                    : AppColors.inputsColor,
-
-                hintText: context.tr('search_for_event'),
-                hintStyle: Theme.of(context).textTheme.labelSmall,
-
-                errorBorderColor: AppColors.redColor,
-                generalBorderColor: context.isLight
-                    ? AppColors.strokeColor
-                    : AppColors.strokeDarkColor,
-              ),
-              SizedBox(height: context.height * 0.004),
-              Text(
-                context.tr('event_title'),
-                style: Theme.of(context).textTheme.labelLarge,
-              ),
-              CustomTextField(
-                validator: (text) {
-                  if (text?.trim().isEmpty ?? true) {
-                    return context.tr('please_enter_event_description');
-                  }
-                  return null;
-                },
-                onChanged: (text) {
-                  eventDescription = text;
-                },
-                maxLines: 6,
-                dataStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  decoration: TextDecoration.none,
+                DateTimeWidget.time(
+                  onPressed: pickTime,
+                  hyperText: selectedTime == null
+                      ? context.tr('choose_time')
+                      : selectedTime!.format(context),
                 ),
-                filled: true,
-                fillColor: context.isLight
-                    ? AppColors.whiteColor
-                    : AppColors.inputsColor,
+                SizedBox(height: context.height * 0.01),
 
-                hintText: context.tr('event_description'),
-                hintStyle: Theme.of(context).textTheme.labelSmall,
-
-                errorBorderColor: AppColors.redColor,
-                generalBorderColor: context.isLight
-                    ? AppColors.strokeColor
-                    : AppColors.strokeDarkColor,
-              ),
-              SizedBox(height: context.height * 0.004),
-
-              DateTimeWidget.date(
-                onPressed: pickDate,
-                hyperText: selectedDate == null
-                    ? context.tr('choose_date')
-                    : DateFormat(
-                        'MMM d, yyyy',
-                        context.locale.toString(),
-                      ).format(selectedDate!),
-              ),
-              DateTimeWidget.time(
-                onPressed: pickTime,
-                hyperText: selectedTime == null
-                    ? context.tr('choose_time')
-                    : selectedTime!.format(context),
-              ),
-              SizedBox(height: context.height * 0.01),
-
-              CustomElevatedButton(
-                onPressed: addEvent,
-                backgroundColor: context.isLight
-                    ? AppColors.mainColor
-                    : AppColors.mainDarkModeColor,
-                child: Text(
-                  context.tr('add_event'),
-                  style: AppStyles.sBold20White,
+                CustomElevatedButton(
+                  onPressed: addEvent,
+                  backgroundColor: context.isLight
+                      ? AppColors.mainColor
+                      : AppColors.mainDarkModeColor,
+                  child: Text(
+                    context.tr('add_event'),
+                    style: AppStyles.sBold20White,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -240,6 +242,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
       initialDate: DateTime.now(),
     );
     selectedDate = chooseDate;
+    FocusManager.instance.primaryFocus?.unfocus();
     setState(() {});
   }
 
@@ -249,6 +252,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
       initialTime: TimeOfDay.now(),
     );
     selectedTime = chooseTime;
+    FocusManager.instance.primaryFocus?.unfocus();
     setState(() {});
   }
 
